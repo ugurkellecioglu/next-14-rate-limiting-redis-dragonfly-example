620ff03e082f8324a9c5aeb62653aab42d0774e4
Ugur <ugur.kellecioglu@outlook.com>
2024-03-23 12:42:16 +0300

    Refactor UpdateButton component to handle API response and display toast

--- components/UpdateButton.tsx
+++ components/UpdateButton.tsx
@@ -5,6 +5,7 @@ import { Button } from "./ui/button"
 import { todo } from "@/lib/schema"
 import { eq } from "drizzle-orm"
 import { updateTodo } from "@/actions"
+import { toast } from "./ui/use-toast"
 
 export default async function UpdateButton({
   id,
@@ -13,7 +14,14 @@ export default async function UpdateButton({
   id: number
   completed: boolean
 }) {
-  return (
-    <Button onClick={async () => updateTodo(id, completed)}>Update Todo</Button>
-  )
+  const onClick = async () => {
+    const res = await updateTodo(id, completed)
+    console.log("res", res)
+    toast({
+      description: res?.message,
+      variant: res?.success ? "default" : "destructive",
+    })
+  }
+
+  return <Button onClick={onClick}>Update Todo</Button>
 }
